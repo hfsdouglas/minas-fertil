6a65bb5d61f02d64f828dd1a5cfeaa1262f93321
Douglas Faria <douglas.faria@proxyerp.com.br>
2023-03-19 18:42:03 -0300

    release: desenvolvimento de contatos e footer

--- scripts/index.js
+++ scripts/index.js
@@ -21,3 +21,12 @@ anchorElemnts.forEach(element => {
     });
 })
 
+const map = document.getElementById('contato');             
+let element = document.createElement('div');
+element.classList.add('mt-4');
+
+element.innerHTML = `
+    <iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" id="gmap_canvas" src="https://maps.google.com/maps?width=520&amp;height=400&amp;hl=en&amp;q=Fazenda%20Brejo%20Preto,%20Zona%20Rural%20-%20Vila%20Comunheira%20C%C3%B3rrego%20Fundo+(MINAS%20FERTIL)&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
+`
+map.append(element);
+
